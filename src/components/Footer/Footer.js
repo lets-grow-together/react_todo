5af65a233676735666652fc353556f7942038b14
zestairlove <violetdessin@gmail.com>
2019-05-11 16:44:45 +0900

    add filter/clear

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,17 +7,36 @@ const cx = classNames.bind(styles);
 
 class Footer extends Component {
   render() {
+    const {
+      selectedFilter,
+      activeLength,
+      shouldClearCompletedShow,
+      onFilterChange,
+      onClearCompleted
+    } = this.props;
+
+    const filterNames = ['All', 'Active', 'Completed'];
+    const filteritems = filterNames.map(filterName => (
+      <li
+        key={`filter_${filterName}`}
+        className={cx({ 'is-selected': filterName === selectedFilter })}
+      >
+        <a href="#none" onClick={() => onFilterChange(filterName)}>
+          {filterName}
+        </a>
+      </li>
+    ));
+
     return (
       <footer>
         <span className={cx('todo-count')}>
-          <strong>3</strong> items left
+          <strong>{activeLength}</strong> item{activeLength === 1 ? '' : 's'} left
         </span>
-        <ul className={cx('todo-filters')}>
-          <li className={cx('is-selected')}><a href="#none">All</a></li>
-          <li><a href="#none">Active</a></li>
-          <li><a href="#none">Completed</a></li>
-        </ul>
-        <button className={cx('todo-clear')}>
+        <ul className={cx('todo-filters')}>{filteritems}</ul>
+        <button
+          className={cx('todo-clear', { 'is-show': shouldClearCompletedShow})}
+          onClick={onClearCompleted}
+        >
           Clear Completed
         </button>
       </footer>
